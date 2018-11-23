074616c0d7a40f3e79b91522c18e975c0399c5fd
neldnelaps <neldnelaps@gmail.com>
2018-11-23 14:23:03 +0700

    delete api

--- TestLoymax/Models/Settings.cs
+++ TestLoymax/Models/Settings.cs
@@ -2,7 +2,7 @@
 {
     public static class Settings
     {
-        public static string ApiToken { get; set; } = "796907636:AAEF2Oen8yzu5qwrDfkyU3mn5I8Un2kELRM";
+        public static string ApiToken { get; set; } = "";
         public static string Url { get; set; } = "https://testloymax.azurewebsites.net:443/{0}";
     }
 }
